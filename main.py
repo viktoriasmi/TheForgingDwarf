242ae894b0e89121cb48c80b5f8126a18a840230
Victoria <viktoria.smi@mail.ru>
2024-02-17 18:52:12 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import psycopg2
 
+con = psycopg2.connect(database="TheForgingDwarf", user="postgres", password="1111", port="5432", host="localhost")
+cur = con.cursor()
+
+#print("Информация о сервере PostgreSQL")
+#print(con.get_dsn_parameters(), "\n")
+#cur.execute("SELECT version();")
+#record = cur.fetchone()
+#print("Вы подключены к - ", record, "\n")
+
+
+
+
+cur.close()
+con.close()
