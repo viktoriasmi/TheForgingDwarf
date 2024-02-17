ed4913be8495d50f3b29647016321f0836dc19ef
Victoria <viktoria.smi@mail.ru>
2024-02-17 20:07:57 +0300

    database creation

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import psycopg2
 
-con = psycopg2.connect(database="TheForgingDwarf", user="postgres", password="1111", port="5432", host="localhost")
+# con = psycopg2.connect(database="postgres", user="postgres", password="1111", port="5432", host="localhost")
+con = psycopg2.connect(database="theforgingdwarf", user="postgres", password="1111", port="5432", host="localhost")
 cur = con.cursor()
+#con.autocommit = True
+#cur.execute("CREATE DATABASE TheForgingDwarf;")
 
-#print("Информация о сервере PostgreSQL")
-#print(con.get_dsn_parameters(), "\n")
-#cur.execute("SELECT version();")
-#record = cur.fetchone()
-#print("Вы подключены к - ", record, "\n")
+
+# print("Информация о сервере PostgreSQL")
+# print(con.get_dsn_parameters(), "\n")
+# cur.execute("SELECT version();")
+# record = cur.fetchone()
+# print("Вы подключены к - ", record, "\n")
 
 
 
